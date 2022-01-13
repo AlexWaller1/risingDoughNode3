2695333565da2a05d8f69a9096e295fc573825e8
Alex Waller <alex@artofalexwaller.com>
2022-01-12 21:17:51 -0500

    extensionType()

--- reference/pathPractice.js
+++ reference/pathPractice.js
@@ -1,10 +1,8 @@
 // pathPractice.js
 
 console.log(__filename);
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/pathPractice.js
 
 console.log(__dirname);
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference
 
 const path = require("path");
 // must always require "path" to gain access to
@@ -14,7 +12,7 @@ console.log(path.basename(__filename));
 // pathPractice.js
 
 console.log(path.dirname(__filename));
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference
+
 // this is the same as logging the __dirname directly
 
 // file extension
@@ -28,7 +26,7 @@ console.log(path.parse(__filename));
 
 {
    root: '/',
-   dir: '/Users/alexwaller/codeMentorPractice/risingDoughNode3/reference',
+   dir: 
    base: 'pathPractice.js',
    ext: '.js',
    name: 'pathPractice
@@ -43,10 +41,8 @@ console.log(path.parse(__filename).root);
 //       /
 
 console.log(path.parse(__filename).dir);
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference
 
 console.log(__dirname);
-// /Users/ alexwaller/codeMentorPractice/risingDoughNode3/reference
 
 console.log(path.parse(__filename).base);
 // pathPractice.js
@@ -59,22 +55,19 @@ console.log(path.parse(__filename).name);
 
 // Concatenate paths
 console.log(path.join(__dirname, "test", "hello.html"));
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/test/hello.html
 
 const mellonTechPath = path.join(__dirname, "robots", "mellonTech.js");
 
 console.log(mellonTechPath);
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robots/mellonTech.js
 
 console.log(path.dirname(mellonTechPath));
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robots
 
 console.log(path.parse(mellonTechPath));
 /* 
 
 {
     root: '/',
-    dir: '/Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robots',
+    dir: 
     base: 'mellonTech.js',
     ext: '.js',
     name: 'mellonTech'
@@ -86,10 +79,8 @@ console.log(path.parse(mellonTechPath).root);
 // /
 
 console.log(path.parse(mellonTechPath).dir);
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robots
 
 console.log(path.dirname(mellonTechPath));
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robots
 
 console.log(path.parse(mellonTechPath).base);
 // mellonTech.js
@@ -109,7 +100,85 @@ console.log(typeof path.parse(mellonTechPath).ext);
 const hank44Path = path.join(__dirname, "robot", "hank_44.css");
 
 console.log(hank44Path);
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robot/hank_44.js
 
 console.log(path.dirname(hank44Path));
-// /Users/alexwaller/codeMentorPractice/risingDoughNode3/reference/robot
+
+console.log(path.extname(hank44Path));
+// .css
+
+console.log(path.parse(hank44Path));
+/* 
+
+{
+    root: '/',
+    dir: 
+    base: 'hank_44.css',
+    ext: '.css',
+    name: 'hank_44'
+}
+
+*/
+
+console.log(path.parse(hank44Path).base);
+// hank_44.css
+
+console.log(path.parse(hank44Path).ext);
+// .css
+
+console.log(path.parse(hank44Path).name);
+// hank_44
+
+console.log(typeof hank44Path == "string");
+// true
+
+console.log(typeof hank44Path);
+// string
+
+const greenEgg = path.join(__dirname, "reference", "robot", "greenEgg.html");
+
+console.log(greenEgg);
+
+const greenEgg2 = path.join(__dirname, "robot", "greenEgg.html");
+
+console.log(greenEgg2);
+
+console.log(path.parse(greenEgg2));
+/* 
+
+{
+    root: '/',
+    dir: 
+    base: 'greenEgg.html',
+    ext: '.html'
+    name: 'greenEgg'
+}
+
+*/
+
+console.log(path.parse(greenEgg2).name);
+// greenEgg
+
+console.log(path.parse(greenEgg2).ext);
+// .html
+
+function extensionType(pathName) {
+  switch (path.parse(pathName).ext) {
+    case ".html":
+      return `${path.parse(pathName).name} has a html extension`;
+
+    case ".js":
+      return `${path.parse(pathName).name} has a JavaScript extension`;
+
+    case ".css":
+      return `${path.parse(pathName).name} has a css extension`;
+  }
+}
+
+console.log(extensionType(greenEgg2));
+// greenEgg has a html extension
+
+console.log(extensionType(mellonTechPath));
+// mellonTech has a JavaScript extension
+
+console.log(extensionType(hank44Path));
+// hank_44 has a css extension
